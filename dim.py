e00a4fc2a9765fdb93f21dc3cf46145fbd1914e1
niuyuhang03 <niuyuhang03@qq.com>
2019-08-22 20:00:06 +0800

    update .slurm

--- dim.py
+++ dim.py
@@ -14,18 +14,18 @@ with open('data/FB15k-237/test.txt') as f:
     print(len(lines))
 print('\n')
 
-with open('data/FB15k-237_new/train.txt') as f:
-    print("FB15k-237_new/train.txt:")
+with open('data/FB15k-237/train.txt') as f:
+    print("FB15k-237/train.txt:")
     lines = f.readlines()
     print(len(lines))
 
-with open('data/FB15k-237_new/valid.txt') as f:
-    print("FB15k-237_new/valid.txt:")
+with open('data/FB15k-237/valid.txt') as f:
+    print("FB15k-237/valid.txt:")
     lines = f.readlines()
     print(len(lines))
 
-with open('data/FB15k-237_new/test.txt') as f:
-    print("FB15k-237_new/test.txt:")
+with open('data/FB15k-237/test.txt') as f:
+    print("FB15k-237/test.txt:")
     lines = f.readlines()
     print(len(lines))
 print('\n')
@@ -46,34 +46,34 @@ with open('data/WN18RR/test.txt') as f:
     print(len(lines))
 print('\n')
 
-with open('data/WN18RR_new/train.txt') as f:
-    print("WN18RR_new/train.txt:")
+with open('data/WN18RR/train.txt') as f:
+    print("WN18RR/train.txt:")
     lines = f.readlines()
     print(len(lines))
 
-with open('data/WN18RR_new/valid.txt') as f:
-    print("WN18RR_new/valid.txt:")
+with open('data/WN18RR/valid.txt') as f:
+    print("WN18RR/valid.txt:")
     lines = f.readlines()
     print(len(lines))
 
-with open('data/WN18RR_new/test.txt') as f:
-    print("WN18RR_new/test.txt:")
+with open('data/WN18RR/test.txt') as f:
+    print("WN18RR/test.txt:")
     lines = f.readlines()
     print(len(lines))
 print('\n')
 
-with open('data/WN18RR_sub30000_new/train.txt') as f:
-    print("WN18RR_sub30000_new/train.txt:")
+with open('data/WN18RR_sub30000/train.txt') as f:
+    print("WN18RR_sub30000/train.txt:")
     lines = f.readlines()
     print(len(lines))
 
-with open('data/WN18RR_sub30000_new/valid.txt') as f:
-    print("WN18RR_sub30000_new/valid.txt:")
+with open('data/WN18RR_sub30000/valid.txt') as f:
+    print("WN18RR_sub30000/valid.txt:")
     lines = f.readlines()
     print(len(lines))
 
-with open('data/WN18RR_sub30000_new/test.txt') as f:
-    print("WN18RR_sub30000_new/test.txt:")
+with open('data/WN18RR_sub30000/test.txt') as f:
+    print("WN18RR_sub30000/test.txt:")
     lines = f.readlines()
     print(len(lines))
 print('\n')
--- preprocess.sh
+++ preprocess.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-mkdir data
-mkdir data/FB15k-237
-mkdir data/WN18RR
+# mkdir data
+# mkdir data/FB15k-237
+# mkdir data/WN18RR
 mkdir saved_models
 
-tar -xvf WN18RR.tar.gz -C data/WN18RR
-tar -xvf FB15k-237.tar.gz -C data/FB15k-237
+# tar -xvf WN18RR.tar.gz -C data/WN18RR
+# tar -xvf FB15k-237.tar.gz -C data/FB15k-237
 
-python wrangle_KG.py WN18RR
 python wrangle_KG.py FB15k-237
-python wrangle_KG.py FB15k-237_new
-python wrangle_KG.py WN18RR_new
-python wrangle_KG.py WN18RR_sub30000_new
+python wrangle_KG.py FB15k-237_rel
+python wrangle_KG.py WN18RR
+python wrangle_KG.py WN18RR_rel
+python wrangle_KG.py WN18RR_sub30000
+python wrangle_KG.py WN18RR_sub30000_rel
