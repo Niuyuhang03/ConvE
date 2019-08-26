759e87ec12189e7424330de1803159cd5f892915
Niuyuhang03 <1411490323@qq.com>
2019-08-26 15:40:46 +0800

    add data origin

--- preprocess.sh
+++ preprocess.sh
@@ -7,8 +7,10 @@ mkdir saved_models
 # tar -xvf WN18RR.tar.gz -C data/WN18RR
 # tar -xvf FB15k-237.tar.gz -C data/FB15k-237
 
+python wrangle_KG.py FB15k-237_origin
 python wrangle_KG.py FB15k-237
 python wrangle_KG.py FB15k-237_rel
+python wrangle_KG.py WN18RR_origin
 python wrangle_KG.py WN18RR
 python wrangle_KG.py WN18RR_rel
 python wrangle_KG.py WN18RR_sub30000
