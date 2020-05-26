30d35dd0994c016ddc810259470f7a2e6f8ff298
Niuyuhang03 <1411490323@qq.com>
2020-05-27 00:09:07 +0800

    add model

--- preprocess.sh
+++ preprocess.sh
@@ -22,7 +22,7 @@ cp ./data/WN18RR_4000/*.json ./data/WN18RR_4000_rel/
 
 
 cp ../pyGAT/GAT_FB15K237_4000_adsf/20200525-222338/GAT_FB15K237_4000_output.txt ./data/FB15K237_4000_adsf/FB15K237_4000_adsf.content
-cp ../pyGAT/data/FB15K237_4000/FB15K237_4000.rel ./data/FB15K237_4000_adsf/FB15K237_4000_rel.rel
+cp ../pyGAT/data/FB15K237_4000/FB15K237_4000.rel ./data/FB15K237_4000_adsf/FB15K237_4000_adsf.rel
 cp ./data/FB15K237_4000/*.json ./data/FB15K237_4000_adsf/
 
 cp ../pyGAT/GAT_WN18RR_4000_adsf/20200525-234519/GAT_WN18RR_4000_output.txt ./data/WN18RR_4000_adsf/WN18RR_4000_adsf.content
@@ -31,7 +31,7 @@ cp ./data/WN18RR_4000/*.json ./data/WN18RR_4000_adsf/
 
 
 cp ../pyGAT/GAT_FB15K237_4000_all/20200520-104745/GAT_FB15K237_4000_output.txt ./data/FB15K237_4000_all/FB15K237_4000_all.content
-cp ../pyGAT/data/FB15K237_4000/FB15K237_4000.rel ./data/FB15K237_4000_all/FB15K237_4000_rel.rel
+cp ../pyGAT/data/FB15K237_4000/FB15K237_4000.rel ./data/FB15K237_4000_all/FB15K237_4000_all.rel
 cp ./data/FB15K237_4000/*.json ./data/FB15K237_4000_all/
 
 cp ../pyGAT/GAT_WN18RR_4000_all/20200520-104941/GAT_WN18RR_4000_output.txt ./data/WN18RR_4000_all/WN18RR_4000_all.content
